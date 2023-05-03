d2a23d224e397656a1f4857d8afff1f7dc971359
ramaabalaji <ramaa_t@yahoo.com>
2023-05-03 16:53:09 +0100

    including one hot encoding for categorical

--- utils.py
+++ utils.py
@@ -1,45 +1,116 @@
-import pandas as pd 
+"""importing all necessary packages here citing XAI christoph Molnar for credits."""
+# importing all necessary packages here citing XAI christoph Molnar for credits
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
 # Makes sure we see all columns
 pd.set_option('display.max_columns', None)
-from sklearn.model_selection import train_test_split
-from imblearn.over_sampling import RandomOverSampler
-
-class DataLoader():
+class LoadDatafromCSV():
+    # dander method used as a constructor
     def __init__(self):
         self.data = None
+
+# Loading Data from the csv file from kaggle
 
     def load_dataset(self, path="data/healthcare-dataset-stroke-data.csv"):
         self.data = pd.read_csv(path)
 
+    def prep_data(self):
+
+        # Begin the EDa process by learning about your data
+        # The data contains categorical values which should be converted
+        # before we perform any operations on it. We use One hot encoding
+        # to these variables for simplicity
+        # One-hot encode all categorical columns
+        
+        # Impute missing values of BMI with 0 if it is not provided already.
+        self.data.bmi = self.data.bmi.fillna(0)
+        
     def preprocess_data(self):
+
+        # Begin the EDa process by learning about your data 
+        # The data contains categorical values which should be converted 
+        # before we perform any operations on it. We use One hot encoding 
+        # to these variables for simplicity  
         # One-hot encode all categorical columns
         categorical_cols = ["gender",
                             "ever_married",
                             "work_type",
                             "Residence_type",
                             "smoking_status"]
-        encoded = pd.get_dummies(self.data[categorical_cols], 
-                                prefix=categorical_cols)
+        
+        ohe = OneHotEncoder(categories='auto', 
+                    drop='first',sparse_output=False)
+        
+        encoded = pd.DataFrame(ohe.fit_transform(self.data[categorical_cols]))        
 
+        #encoded = pd.get_dummies(self.data[categorical_cols], 
+        #                    prefix=categorical_cols)
+
+        # Add the encoded values back into the dataframe and 
+        # drop the first value out of the dataframe in order to reduce the correlations 
         # Update data with new columns
         self.data = pd.concat([encoded, self.data], axis=1)
         self.data.drop(categorical_cols, axis=1, inplace=True)
 
-        # Impute missing values of BMI
+        # Impute missing values of BMI with 0 if it is not provided already.
+        self.data.bmi = self.data.bmi.fillna(0)
+        
+        # Drop id as it is not relevant
+        self.data.drop(["id"], axis=1, inplace=True)
+
+
+        # Standardization 
+        # Usually we would standardize here and convert it back later 
+        # This is done in order to keep all parameters to same scale so model does not get baised  
+        # But for simplification we will not standardize / normalize the features
+
+    def preprocess_data_dummy(self):
+
+        # Begin the EDa process by learning about your data 
+        # The data contains categorical values which should be converted 
+        # before we perform any operations on it. We use One hot encoding 
+        # to these variables for simplicity  
+        # One-hot encode all categorical columns
+        categorical_cols = ["gender",
+                            "ever_married",
+                            "work_type",
+                            "Residence_type",
+                            "smoking_status"]
+        
+        encoded = pd.get_dummies(self.data[categorical_cols], 
+                            prefix=categorical_cols)
+
+        # Add the encoded values back into the dataframe and 
+        # drop the first value out of the dataframe in order to reduce the correlations 
+        # Update data with new columns
+        self.data = pd.concat([encoded, self.data], axis=1)
+        self.data.drop(categorical_cols, axis=1, inplace=True)
+
+        # Impute missing values of BMI with 0 if it is not provided already.
         self.data.bmi = self.data.bmi.fillna(0)
         
         # Drop id as it is not relevant
         self.data.drop(["id"], axis=1, inplace=True)
 
         # Standardization 
-        # Usually we would standardize here and convert it back later
+        # Usually we would standardize here and convert it back later 
+        # This is done in order to keep all parameters to same scale so model does not get baised  
         # But for simplification we will not standardize / normalize the features
+
 
     def get_data_split(self):
         X = self.data.iloc[:,:-1]
         y = self.data.iloc[:,-1]
-        return train_test_split(X, y, test_size=0.20, random_state=2021)
+
+        # 75 25 split for training and testing 
+        return train_test_split(X, y, test_size=0.25, random_state=2021)
     
+    # Since the stoke data is an imbalanced data set
+    # we are performing oversmapling here for the minortity class. 
+    # We could also do SMOTE instead of oversampling
     def oversample(self, X_train, y_train):
         oversample = RandomOverSampler(sampling_strategy='minority')
         # Convert to numpy and oversample
