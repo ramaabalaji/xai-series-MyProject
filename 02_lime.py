d17b4b1bafc5028e89d750273922f0018192213b
ramaabalaji <ramaa_t@yahoo.com>
2023-05-17 11:57:29 +0100

    Update 02_lime.py

--- 02_lime.py
+++ 02_lime.py
@@ -3,14 +3,15 @@
 # importing all necessary packages here citing XAI christoph Molnar for credits
 # The base code is derived from the above and necesssary changes based on my learning has been 
 # implemented here. 
+import warnings
+warnings.filterwarnings('ignore')
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score, accuracy_score
 import lime
 import lime.lime_tabular
 from lime import submodular_pick
 
-import warnings
-warnings.filterwarnings('ignore')
 from interpret.blackbox import LimeTabular
 from interpret import show
 
@@ -42,11 +43,11 @@ X_train, X_test, y_train, y_test = data_loader.get_data_split()
 # The best way is to use SMOTE for sampling the data. 
 # Pipeline has been used to combine, SMOTE - oversampling for minority data and 
 # undersampling for the majority class. 
-
-X_train, y_train = data_loader.smotesample(X_train, y_train)
 print(X_train.shape)
 print(X_test.shape)
-print("After smotesampling: ---------------------")
+
+X_train, y_train = data_loader.smotesample(X_train, y_train)
+print("After smotesampling: ---------------------",X_train.shape)
 
 
 # %% Fit blackbox model
@@ -66,11 +67,10 @@ explainer = lime.lime_tabular.LimeTabularExplainer(
     X_train.values, 
     feature_names=X_train.columns.astype(str).values.tolist(),
     class_names=['MEDV'], verbose=True, mode='regression')
-exp = explainer.explain_instance(X_test.values[12], rf.predict, num_features=10)
+exp = explainer.explain_instance(X_test.values[12], rf.predict, 
+                                 num_features=10)
 exp.show_in_notebook(show_table=True)
 exp.as_list()
-
-"""
 
 lime = LimeTabular(model=rf.predict_proba,
                    data=X_train, 
@@ -83,9 +83,7 @@ lime_local = lime.explain_local(X_test[-20:],
 show(lime_local)
 
 
-
-
-
+"""
 model = XGBClassifier(n_estimators = 300, random_state = 123)
 model.fit(X_train, y_train)
 model.score(X_test, y_test)
@@ -93,17 +91,17 @@ predict_fn = lambda x: model.predict_proba(x)
 np.random.seed(123)
 # Defining the LIME explainer object
 
-feature_names=X_train.columns.astype(str).values.tolist(),
-explainer = lime.lime_tabular.LimeTabularExplainer(X_train.values,
-                                                   mode='classification',
-                                                   class_names=['No Stroke', 'Stroke'],
-                                                   training_labels='Stroke',
-                                                   feature_names=feature_names)
+feature_names=X_train.columns.astype(str).values
+explainer = lime.lime_tabular.LimeTabularExplainer(
+                                        X_train.values,
+                                        mode='classification',
+                                        class_names= ['Stroke'],
+                                        training_labels='Stroke',
+                                        feature_names=feature_names)
 # Local Interpretability on particular instance
 # using LIME to get the explanations
-i = 12
+i = 10
 exp = explainer.explain_instance(X_train.loc[i,X_train.columns].astype(str).values, predict_fn, num_features=21)
 exp.show_in_notebook(show_table=True)# %%
-
 
 """
